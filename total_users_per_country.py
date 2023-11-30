bbd07b6a4de7c031bbb03f1da9b609f4f3495e44
Samuel Vítek <samuel.vitek@email.cz>
2023-11-30 19:37:45 +0200

    refactor: conversion to subreport

--- total_users_per_country.py
+++ total_users_per_country.py
@@ -3,6 +3,7 @@ from ast import literal_eval
 
 import pandas as pd
 import plotly.graph_objs as go
+from plotly.subplots import make_subplots
 
 # Display options in order not to be constrained in some cases of printing
 pd.set_option('display.max_columns', 15)
@@ -26,20 +27,41 @@ account['country_name'] = [data[k] for k in account['country_code']]
 # Grouping data by country_name and counting how many account_ids (unique values) occurred in each country
 users_by_country = account.groupby('country_name')['account_id'].count()
 users_by_country = users_by_country.reset_index()
+users_by_country = users_by_country.sort_values(by='account_id', ascending=False)
 
 # Plot with trend line using 'lowess' (data smoothing)
 # locations - each country recorded in country_names column
 # z - total amounts of users in each country
-data = dict(type='choropleth',
-            locations=users_by_country['country_name'],
-            locationmode='country names',
-            colorscale='Portland',
-            text=['Country'],
-            z=users_by_country['account_id'],
-            colorbar={'title': 'Amount of active users'})
+map_data = go.Figure(go.Choropleth(
+    locations=users_by_country['country_name'],
+    locationmode='country names',
+    colorscale='Portland',
+    text=['Country'],
+    z=users_by_country['account_id'],
+    colorbar={'title': 'Total active users'}
+))
 
-layout = dict(title='Total amount of users in each country throughout the year 2016',
-              geo=dict(projection={'type': 'mercator'})
-              )
-users_map = go.Figure(data=[data], layout=layout)
-users_map.show()
+bar_chart_data = go.Figure(go.Bar(
+    x=users_by_country['account_id'].nlargest(n=10),
+    y=users_by_country['country_name'],
+    hovertemplate='Total users: %{x}, Country: %{y} <extra></extra>',
+    marker=dict(color=users_by_country['account_id'],
+                colorscale='Portland'),
+    orientation='h'))
+
+# Create Subplot with two columns and one row
+subplot = make_subplots(rows=1, cols=2,
+                        specs=[[{'type': 'choropleth'}, {'type': 'xy'}]])
+
+# Add traces from figures to subplot
+for trace in map_data.data:
+    subplot.add_trace(trace, row=1, col=1)
+
+for trace in bar_chart_data.data:
+    subplot.add_trace(trace, row=1, col=2)
+
+# Update and show layout of the Subplot
+subplot.update_layout(title_text="Total amount of users in each country throughout the year 2016",
+                      yaxis={'categoryorder': 'total ascending'},
+                      margin=dict(l=60, r=60, t=50, b=50))
+subplot.show()
